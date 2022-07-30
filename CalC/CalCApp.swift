ea73aa203f046c006a12ed650a859014b5d67b09
ravi-solutelabs <ravi.g@solutelabs.com>
2022-07-30 11:48:30 +0530

    Initial Commit

---
+++ CalC/CalCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalCApp.swift
+//  CalC
+//
+//  Created by Ravi on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CalCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
